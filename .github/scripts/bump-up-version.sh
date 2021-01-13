ac1f5cb312a77749fbe6ab38e9f0bcfd94188648
hasancelik <hasan@hazelcast.com>
2021-01-13 18:42:40 +0300

    snapshot helm chart based on latest-snapshot IMDG and MC images

---
+++ .github/scripts/bump-up-version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+version=$1
+type=$2
+
+IFS=. read -r major minor patch <<<"$version"
+
+# $ ./bump-up-version.sh 4.1.1 app
+# 4.2-SNAPSHOT
+# $ ./bump-up-version.sh 3.5.2 chart
+# 3.5.3-snapshot
+if [ $type = app ]; then
+    ((minor++))
+    printf -v version '%d.%d-SNAPSHOT' "$major" "$((minor))"
+elif [ $type = chart ]; then
+    ((patch++))
+    printf -v version '%d.%d.%d-snapshot' "$major" "$minor" "$((patch))"
+fi
+
+echo $version
