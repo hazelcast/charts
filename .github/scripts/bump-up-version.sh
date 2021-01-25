adb2eaf9920368d0b71fb8db6ef39c1289281b87
hasancelik <hasan@hazelcast.com>
2021-01-25 12:16:09 +0300

    adress PR reviews

--- .github/scripts/bump-up-version.sh
+++ .github/scripts/bump-up-version.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# $ ./bump-up-version.sh 4.1.1 app
+# 4.2-SNAPSHOT
+# $ ./bump-up-version.sh 3.5.2 chart
+# 3.5.3-latest-snapshot
+
 version=$1
 type=$2
 
 IFS=. read -r major minor patch <<<"$version"
 
-# $ ./bump-up-version.sh 4.1.1 app
-# 4.2-SNAPSHOT
-# $ ./bump-up-version.sh 3.5.2 chart
-# 3.5.3-latest-snapshot
 if [ $type = app ]; then
     ((minor++))
     printf -v version '%d.%d-SNAPSHOT' "$major" "$((minor))"
